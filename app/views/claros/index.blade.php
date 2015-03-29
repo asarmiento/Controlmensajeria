e39fd610e212603568b96592352c3eae0011c6c3
Francisco Gamonal <hfgamonalb@gmail.com>
2015-03-29 13:47:43 -0500

    Login Systema

--- app/views/claros/index.blade.php
+++ app/views/claros/index.blade.php
@@ -1,47 +1,43 @@
-	@extends('template.main')
+@extends('template.base')
+
 @section('head')
-<meta name="description" content="Pagina inicio">
-<meta name="author" content="Sistemas Amigables">
-<meta name="keyword" content="palabras, clave">     
-<title>Estado Cuenta</title>
-
+	<meta name="description" content="Estado de Cuenta">
+	<meta name="author" content="Sistemas Amigables">
+	<title>Estado Cuenta</title>
 @stop
 
-@section('title')
-<h1 class="text-lowercase"></h1>
-@stop
-
-@section('container') 
-     
-<center><h2><span class="glyphicon glyphicon-list-alt"><br>Administración Claro <img src="http://sistema.elcorso.hn/asset/img/logosclientes/logo-claro.png"></span></h2></center>
-<hr>
-@if(Auth::user()->type_users_id=="1")
-<h3><span class="glyphicon glyphicon-list-alt"><br>Ciclos</span></h3>
-<div class="btn btn-danger"><a href="{{route('importar-ciclo',1)}}">Subir Ciclo</a></a></div>
-<div class="btn btn-danger"><a href="claros/productos">Productos</a></a></div>
-<div class="btn btn-danger"><a href="claros/agregar">Agregar Personas al Ciclo</a></a></div>
-<div class="btn btn-danger"><a href="{{route('historial-productos',1)}}">Historial de Ciclo</a></div>
-<hr>
-<h3><span class="glyphicon glyphicon-list-alt"><br>Scanear</span></h3>
-<div class="btn btn-danger"><a href="claros/scanearc48">Scanear Sobres <br>de Ciclo C-48</a></div>
-<div class="btn btn-danger"><a href="claros/scanearc46tv">Scanear Sobres <br>de Ciclo C-46 TV</a></div>
-<div class="btn btn-danger"><a href="claros/scanearc46movil">Scanear Sobres <br>de Ciclo C-46 Movil</a></div>
-<div class="btn btn-danger"><a href="claros/scanearmala">Scanear Devolución<br>por mala dirección</a></div>
-<div class="btn btn-danger"><a href="claros/scanearcambio">Scanear Devolución<br>por Cambio de dirección</a></div>
-<div class="btn btn-danger"><a href="claros/scanearcentro">Scanear Devolución<br>por Cambio de centro de Trabajo</a></div>
-<hr>
-<h3><span class="glyphicon glyphicon-list-alt"><br>Otras funciones</span></h3>
-<div class="btn btn-danger"><a href="claros/barrido">Barrido</a></div>
-@endif
-@if(Auth::user()->type_users_id=="2")
-
-<h3><span class="glyphicon glyphicon-list-alt"><br>Scanear</span></h3>
-<div class="btn btn-danger"><a href="claros/scanearc48">Scanear Sobres <br>de Ciclo C-48</a></div>
-<div class="btn btn-danger"><a href="claros/scanearc46tv">Scanear Sobres <br>de Ciclo C-46 TV</a></div>
-<div class="btn btn-danger"><a href="claros/scanearc46movil">Scanear Sobres <br>de Ciclo C-46 Movil</a></div>
-<div class="btn btn-danger"><a href="claros/scanearmala">Scanear Devolución<br>por mala dirección</a></div>
-<div class="btn btn-danger"><a href="claros/scanearcambio">Scanear Devolución<br>por Cambio de dirección</a></div>
-<div class="btn btn-danger"><a href="claros/scanearcentro">Scanear Devolución<br>por Cambio de centro de Trabajo</a></div>
-
-@endif
+@section('content') 
+	<center>
+		<h2>
+			<span class="glyphicon glyphicon-list-alt"></span>Administración Claro <img src="{{asset('img/logo-claro.png')}}">
+		</h2>
+	</center>
+	<hr>
+	@if(Auth::user()->type_users_id=="1")
+		<h3><span class="glyphicon glyphicon-list-alt"></span>Ciclos</h3>
+		<a class="btn btn-danger"href="{{route('importar-ciclo',1)}}">Subir Ciclo</a>
+		<a class="btn btn-danger"href="claros/productos">Productos</a>
+		<a class="btn btn-danger"href="claros/agregar">Agregar Personas al Ciclo</a>
+		<a class="btn btn-danger"href="{{route('historial-productos',1)}}">Historial de Ciclo</a>
+		<hr>
+		<h3><span class="glyphicon glyphicon-list-alt"></span>Scanear</h3>
+		<a class="btn btn-danger"href="claros/scanearc48">Scanear Sobres <br>de Ciclo C-48</a>
+		<a class="btn btn-danger"href="claros/scanearc46tv">Scanear Sobres <br>de Ciclo C-46 TV</a>
+		<a class="btn btn-danger"href="claros/scanearc46movil">Scanear Sobres <br>de Ciclo C-46 Movil</a>
+		<a class="btn btn-danger"href="claros/scanearmala">Scanear Devolución<br>por mala dirección</a>
+		<a class="btn btn-danger"href="claros/scanearcambio">Scanear Devolución<br>por Cambio de dirección</a>
+		<a class="btn btn-danger"href="claros/scanearcentro">Scanear Devolución<br>por Cambio de centro de Trabajo</a>
+		<hr>
+		<h3><span class="glyphicon glyphicon-list-alt"></span>Otras funciones</h3>
+		<a class="btn btn-danger"href="claros/barrido">Barrido</a>
+	@endif
+	@if(Auth::user()->type_users_id=="2")
+		<h3><span class="glyphicon glyphicon-list-alt"></span>Scanear</h3>
+		<a class="btn btn-danger"href="claros/scanearc48">Scanear Sobres <br>de Ciclo C-48</a>
+		<a class="btn btn-danger"href="claros/scanearc46tv">Scanear Sobres <br>de Ciclo C-46 TV</a>
+		<a class="btn btn-danger"href="claros/scanearc46movil">Scanear Sobres <br>de Ciclo C-46 Movil</a>
+		<a class="btn btn-danger"href="claros/scanearmala">Scanear Devolución<br>por mala dirección</a>
+		<a class="btn btn-danger"href="claros/scanearcambio">Scanear Devolución<br>por Cambio de dirección</a>
+		<a class="btn btn-danger"href="claros/scanearcentro">Scanear Devolución<br>por Cambio de centro de Trabajo</a>
+	@endif
 @stop
--- app/views/claros/listaDatosEmpresas.blade.php
+++ app/views/claros/listaDatosEmpresas.blade.php
@@ -1,100 +1,98 @@
 @extends('template.main')
-@section('head')
-<meta name="description" content="Pagina inicio">
-<meta name="author" content="Sistemas Amigables">
-<meta name="keyword" content="palabras, clave">     
-<title>Estado Cuenta</title>
 
+@section('head')
+    <meta name="description" content="Pagina inicio">
+    <meta name="author" content="Sistemas Amigables">
+    <meta name="keyword" content="palabras, clave">     
+    <title>Estado Cuenta</title>
 @stop
 
-@section('title')
-<h1 class="text-lowercase"></h1>
+@section('styles')
+    {{ HTML::style('css/DT_bootstrap.css') }}
+@stop
+
+@section('scripts')
+    {{ HTML::script('js/DT_bootstrap.js') }}
 @stop
 
 @section('container') 
-     
-<center><h2><span class="glyphicon glyphicon-list-alt"><br>Ciclo C-48 <img src="http://sistema.elcorso.hn/asset/img/logosclientes/logo-claro.png"></span></h2></center>
-
-<div style="center">
-
-
-</div>
-
-<div class="right">
-<form class="navbar-form navbar-left" role="search">
-  <div class="form-group">
-    <input type="text" class="form-control" placeholder="Search">
-  </div>
-  <button type="submit" class="btn btn-default">Buscar</button>
-</form>
-</div>
-
-
-<div class="left">
-    <li class="dropdown">                  
-        <a href="#" class="btn btn-danger" data-toggle="dropdown">Reportes +<span class="caret"></span></a>  
-        <ul class="dropdown-menu" role="menu">                        
-            <li><a>{{ HTML::link('/claros/pdfc48', 'Reporte en PDF',array('target'=>'_black')) }}</a></li>    
-            <li><a>{{ HTML::link('/claros/excelc48', 'Reporte en Excel') }}</a></li>    
-        </ul>               
-    </li>               
-</div>
-<hr>
-<div class="left">
-
-</div>
-<hr>
-<div class="table-responsive"> 
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th width="57">{{('#')}}</th>
-            <th width="211">Código</th>
-            <th width="157">Nombre</th>
-            <th width="157">Tipo Cliente</th>
-            <th width="182">Estado</th>
-            <th width="182">Observación</th>
-            <th width="182">Comentario</th>
-            <th width="182">Mensajero</th>
-            <th width="182">Ciudad</th>
-        </tr>      
-    </thead>
-   <tbody>    
-        <!-- inicio mostramos todos los datos de la tabla catalogo -->
-        <?php $i=0; ?>
-        @foreach($datosEmpresas As $datos) <?php $i++;   ?>
-        <tr>  
-            <td>{{$i}}</td>
-            <td>{{($datos->codigo)}}</td>
-            <td>{{($datos->name_cliente)}}</td>
-            <td>{{($datos->tipo_cliente)}}</td>
-           
-            <td>
-                <div id="" class="">{{($datos->observaciones->estados->name)}}</div>
-                
-            </td>
-            <!-- Observación -->
-            <td><div id="ClaroObservacion_{{$datos->id}}" class="ClaroObservacion">{{($datos->observaciones->name)}}</div>
-                
-            </td>
-            <!-- Comentario -->
-            <td>
-                <div id="recibido-{{$datos->id}}" class="recibido"></div>
-            </td>
-
-            <!-- Empleados -->
-            <td><div id="mensajero_{{$datos->id}}" class="mensajero">@if(($datos->empleados_id==null))  @else {{$datos->empleados->nameCompleto()}} @endif </div>
-                </td>
-                <td>
-                   <div >{{($datos->ciudades->name)}}</div>
-                </td>
-        </tr>
-        @endforeach
-        <!-- Fin de la tabla catalogo -->
-    </tbody>
-</table>
+    <center>
+        <h2>
+            <span class="icon-list-alt"></span>
+            <span>Ciclo C-48</span>
+            <img src="{{asset('img/logo-claro.png')}}">
+        </h2>
+    </center>
+    <div class="span6">
+        <li class="dropdown" style="list-style:none;">                  
+            <a href="#" class="btn btn-danger" data-toggle="dropdown">Reportes +<span class="caret"></span></a>  
+            <ul class="dropdown-menu" role="menu">                        
+                <li><a>{{ HTML::link('/claros/pdfc48', 'Reporte en PDF',array('target'=>'_black')) }}</a></li>    
+                <li><a>{{ HTML::link('/claros/excelc48', 'Reporte en Excel') }}</a></li>    
+            </ul>               
+        </li>    
     </div>
-<div class="pagination">
-    {{$datosEmpresas->appends(array("buscar"=>Input::get("buscar")))->links()}}
-</div>
+        <!-- <div class="span6">
+            <form class="navbar-form navbar-left" role="search">
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Search">
+              </div>
+              <button type="submit" class="btn btn-default">Buscar</button>
+            </form>        
+        </div> -->
+    <hr>
+    <div class="table-responsive"> 
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th width="57">{{('#')}}</th>
+                <th width="211">Código</th>
+                <th width="157">Nombre</th>
+                <th width="157">Tipo Cliente</th>
+                <th width="182">Estado</th>
+                <th width="182">Observación</th>
+                <th width="182">Comentario</th>
+                <th width="182">Mensajero</th>
+                <th width="182">Ciudad</th>
+            </tr>      
+        </thead>
+       <tbody>    
+            <!-- inicio mostramos todos los datos de la tabla catalogo -->
+            <?php $i=0; ?>
+            @foreach($datosEmpresas As $datos) <?php $i++;   ?>
+            <tr>  
+                <td>{{$i}}</td>
+                <td>{{($datos->codigo)}}</td>
+                <td>{{($datos->name_cliente)}}</td>
+                <td>{{($datos->tipo_cliente)}}</td>
+               
+                <td>
+                    <div id="" class="">{{($datos->observaciones->estados->name)}}</div>
+                    
+                </td>
+                <!-- Observación -->
+                <td><div id="ClaroObservacion_{{$datos->id}}" class="ClaroObservacion">{{($datos->observaciones->name)}}</div>
+                    
+                </td>
+                <!-- Comentario -->
+                <td>
+                    <div id="recibido-{{$datos->id}}" class="recibido"></div>
+                </td>
+
+                <!-- Empleados -->
+                <td><div id="mensajero_{{$datos->id}}" class="mensajero">@if(($datos->empleados_id==null))  @else {{$datos->empleados->nameCompleto()}} @endif </div>
+                    </td>
+                    <td>
+                       <div >{{($datos->ciudades->name)}}</div>
+                    </td>
+            </tr>
+            @endforeach
+            <!-- Fin de la tabla catalogo -->
+        </tbody>
+    </table>
+        </div>
+    <div class="pagination">
+        {{$datosEmpresas->appends(array("buscar"=>Input::get("buscar")))->links()}}
+    </div>
 @stop
+
